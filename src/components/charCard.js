e26b84a124abc360d95f8d85863bbd8a36229222
gbeltranl <g.beltranl@uniandes.edu.co>
2022-04-04 23:41:47 -0500

    responsive

--- src/components/charCard.js
+++ src/components/charCard.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 export const CharCard = (props) => {
   const { name, image, gender, status } = props;
   return (
-  <div className= "p-1 col-3">
+  <div className= "p-1 col-md-3 col-xs-12 col-sm-6">
     <Card style={{ width: '18rem' }} key={uuidv4()}>
     <Card.Img variant="top" src={image} />
     <Card.Body>
